30554d5d79dd5480e399a48ae1acd8921203bc6c
rudrabha <rudrabha@gmail.com>
2018-10-20 15:48:29 +0530

    Keypoint matcher updated in util.py

--- lib/util.py
+++ lib/util.py
@@ -28,3 +28,14 @@ def keypoints(img):
 	
 	return kp
 
+def keypoints_orb_matcher(img1, img2):
+	orb = cv.ORB_create()
+	kp1, des1 = orb.detectAndCompute(img1,None)
+	kp2, des2 = orb.detectAndCompute(img2,None)
+	bf = cv.BFMatcher(cv.NORM_L2, crossCheck=True)
+	print (des1)
+	matches = bf.match(des1,des2)
+	matches = sorted(matches, key = lambda x:x.distance)
+	img3=cv.drawMatches(img1,kp1,img2,kp2,matches[0:100], None, flags=2)
+	return img3
+
