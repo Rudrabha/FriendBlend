6a67579374c9e47711b9e82afa55ef1d2f9949c0
rudrabha <rudrabha@gmail.com>
2018-10-26 11:04:20 +0530

    Modified util

--- lib/util.py
+++ lib/util.py
@@ -15,12 +15,12 @@ def detect_body(img):
 	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    list=[]
+	list=[]
 	for (x,y,w,h) in faces: 
 		x1 = int(x-1.5*w)
 		y1 = int(y-h)
 		cv2.rectangle(img,(x1,y1),(x+2*w,img.shape[0]),(255,255,0),2)
-        list.append((x1,y1,x+2*w,img.shape[0]))
+        	list.append((x1,y1,x+2*w,img.shape[0]))
 	return list, img
 
 
@@ -42,16 +42,16 @@ def keypoints_orb_matcher(img1, img2):
 	kp2 = list2pt(k2)
 
 def valid_keypoints(body1,body2,keypoints):
-    op_keypoints = keypoints.copy()
-    top_left_x1,top_left_y1,bot_right_x1,bot_right_y1=body1[0]
-    top_left_x2,top_left_y2,bot_right_x2,bot_right_y2=body2[0]
-    for i in range(len(keypoints)):
-        point = keypoints[i].pt
-        if (((point[0]<top_left_x1 or point[0]>bot_right_x1) or (point[1]<top_left_y1 or point[1]>bot_right_y1)) and ((point[0]<top_left_x2 or point[0]>bot_right_x2) or (point[1]<top_left_y2 or point[1]>bot_right_y2))):
-            continue
-        else:
-            op_keypoints.remove(keypoints[i])
-    return op_keypoints
+	op_keypoints = keypoints.copy()
+	top_left_x1,top_left_y1,bot_right_x1,bot_right_y1=body1[0]
+	top_left_x2,top_left_y2,bot_right_x2,bot_right_y2=body2[0]
+	for i in range(len(keypoints)):
+		point = keypoints[i].pt
+		if (((point[0]<top_left_x1 or point[0]>bot_right_x1) or (point[1]<top_left_y1 or point[1]>bot_right_y1)) and ((point[0]<top_left_x2 or point[0]>bot_right_x2) or (point[1]<top_left_y2 or point[1]>bot_right_y2))):
+			continue
+		else:
+			op_keypoints.remove(keypoints[i])
+	return op_keypoints
 
 def L2_distance(pt1, pt2):
 	return np.linalg.norm(pt1-pt2)
