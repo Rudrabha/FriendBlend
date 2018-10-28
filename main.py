54304adaad05271009e846922b591831dab1ed17
Sangeeth Reddy <reddysangeeth.reddy@gmail.com>
2018-10-28 15:04:38 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import cv2
+from lib.util import *
+
+Image_1 = cv2.imread("data/1.jpg")
+Image_2 = cv2.imread("data/2.jpg")
+
+Image_1 = lab_contrast(Image_1)
+Image_2 = lab_contrast(Image_2)
+
+keypoints_1 = keypoints_orb_detector(Image_1)
+keypoints_2 = keypoints_orb_detector(Image_2)
+
+body_1, img1 = detect_body(Image_1)
+body_2, img2 = detect_body(Image_2)
+
+keypoints_valid_1 = valid_keypoints(body_1,body_2,keypoints_1)
+keypoints_valid_2 = valid_keypoints(body_1,body_2,keypoints_2)
+
+_, descriptor1  = keypoints_orb_descriptor(Image_1,keypoints_valid_1, 10000 )
+_, descriptor2  = keypoints_orb_descriptor(Image_2,keypoints_valid_2, 10000 )
+
+keypoint_matches = keypoint_bf_matcher(descriptor1, descriptor2)
+source_points, destination_points = extract_matched_points(keypoint_matches, keypoints_valid_1, keypoints_valid_2)
+
+homography_matrix = calculate_homography_matrix(source_points, destination_points)
+
+homography_warped = warp_perspective(Image_1.copy(), Image_2.copy(), homography_matrix)
+
+cv2.imwrite("./data/homography_output.jpg", homography_warped)
+
