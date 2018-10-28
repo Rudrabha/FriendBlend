d6d6261045c6de0de02ca3da99f4e289cb33a95b
Sangeeth Reddy <reddysangeeth.reddy@gmail.com>
2018-10-28 15:53:07 +0530

    updated main.py to blend images

--- main.py
+++ main.py
@@ -22,12 +22,16 @@ keypoints_valid_2 = valid_keypoints(body_1,body_2,keypoints_2)
 _, descriptor1  = keypoints_orb_descriptor(Image_1,keypoints_valid_1, 10000 )
 _, descriptor2  = keypoints_orb_descriptor(Image_2,keypoints_valid_2, 10000 )
 
-keypoint_matches = keypoint_bf_matcher(descriptor1, descriptor2)
+keypoint_matches = keypoint_bf_matcher(descriptor1, descriptor2, 37)
+
 source_points, destination_points = extract_matched_points(keypoint_matches, keypoints_valid_1, keypoints_valid_2)
 
 homography_matrix = calculate_homography_matrix(source_points, destination_points)
 
-homography_warped = warp_perspective(Image_1.copy(), Image_2.copy(), homography_matrix)
+homography_warped_1 = warp_perspective(Image_1.copy(), Image_2.copy(), homography_matrix)
 
-cv2.imwrite("./data/homography_output.jpg", homography_warped)
+op_image = 0.5 * homography_warped_1 + 0.5 * Image_2
+
+
+cv2.imwrite("./data/alpha_blend.jpg", op_image)
 
