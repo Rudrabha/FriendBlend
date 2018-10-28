f9655e32b5c015d265faa7f795ad956fcc787a03
rudrabha <rudrabha@gmail.com>
2018-10-28 14:46:54 +0530

    Util Changed

--- lib/util.py
+++ lib/util.py
@@ -12,14 +12,13 @@ def lab_contrast(img, f=7):
 	return op_img
 
 def detect_body(img):
-	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+	face_cascade = cv2.CascadeClassifier('lib/haarcascade_frontalface_default.xml') 
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 	list1=[]
 	for (x,y,w,h) in faces: 
 		x1 = int(x-1.5*w)
 		y1 = int(y-h)
-		#cv2.rectangle(img,(x1,y1),(x+2*w,img.shape[0]),(255,255,0),2)
 		list1.append((x1,y1,x+2*w,img.shape[0]))
 	return list1, img
 
